43ef9586158e976ff40cc6171e78a8f1eac778da
Jason Barry <barryjj@gmail.com>
2018-07-22 13:53:39 -0400

    Added command for macros.   Upping license.txt that was missed in last push.

--- bot.js
+++ bot.js
@@ -1,8 +1,22 @@
-var Discord = require('discord.io');
+var discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 
-var StatusMessageArray = ['Eating stray cats to build energy.',
+const _ = require('lodash');
+
+var macroMap = {
+  headbang : 'https://media2.giphy.com/media/k3M5tUaoz65a0/giphy.gif',
+  badsteve : 'https://i.gifer.com/8oQL.gif',
+  homerhedge: 'https://media.giphy.com/media/3ohs7KViF6rA4aan5u/giphy.gif',
+  wtfhedge: 'https://i.giphy.com/media/3ohs7N0iXvc180MiB2/giphy.webp',
+  grandpahedge: 'https://media.giphy.com/media/2ALDsRipuShI8A3gXV/giphy.gif',
+  dogsoon: 'https://media.giphy.com/media/5xtDarzgzG6eu6uVwI0/giphy.gif',
+  grandpaturnaround: 'https://giphy.com/gifs/fDO2Nk0ImzvvW',
+  howdareyou: 'https://78.media.tumblr.com/tumblr_m64dbowpB71qm7tbzo1_500.gif'
+};
+
+
+var statusMessageArray = ['Eating stray cats to build energy.',
                           'Bathing in the blood of the innocent.', 
                           'Focusing darkness around specified targets.'];
 
@@ -13,8 +27,8 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = 'debug';
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
+// Initialize discord Bot
+var bot = new discord.Client({
    token: auth.token,
    autorun: true
 });
@@ -36,24 +50,58 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'summon':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Currently unable to open a portal to Hell.  Try again later.'
+                    message: "Currently unable to open a portal to Hell.  Try again later."
                 });
             break;
             
             // !status
             case 'status':
-              var i = Math.floor(Math.random() * Math.floor(StatusMessageArray.length));
+              var i = Math.floor(Math.random() * Math.floor(statusMessageArray.length));
                 bot.sendMessage({
                   to: channelID,
-                  message: StatusMessageArray[i]
+                  message: statusMessageArray[i]
                 });
             break;
+
+            // !macro
+            case 'macro':
+              console.log("Macro request made for: " + args[0]);
+              if (_.toLower(args[0]) in macroMap) 
+              {
+                bot.sendMessage({
+                  to: channelID,
+                  message: macroMap[_.toLower(args[0])]
+                });
+              }
+              else if (_.toLower(args[0]) === _.toLower('list'))
+              {
+                macroList = "";
+                for (key in macroMap)
+                {
+                    macroList = macroList + key + "\n";
+                }
+
+                bot.sendMessage({
+                  to: channelID,
+                  message: "Macro list:\n" + macroList
+                });
+
+              }
+              else
+              {
+                bot.sendMessage({
+                  to: channelID,
+                  message: "Macro not recognized. Use !macro list to list available macros."
+                });
+              }
+              break;
+
             
             // unrecognized command
             default:
               bot.sendMessage({
                 to: channelID,
-                message: 'Request outside of the parameters of demonic contract.'
+                message: "Request outside of the parameters of demonic contract."
               });
          }
      }
