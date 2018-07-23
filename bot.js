23805748987180da9a87f442d90028b45cc9d80e
Jason Barry <barryjj@gmail.com>
2018-07-22 21:12:17 -0400

    Convert macroMap population to be read from file until DB is implemented.

--- bot.js
+++ bot.js
@@ -3,19 +3,19 @@ var logger = require('winston');
 var auth = require('./auth.json');
 
 const _ = require('lodash');
+const readline = require('readline');
+const fs = require('fs');
 
-var macroMap = {
-  headbang : 'https://media2.giphy.com/media/k3M5tUaoz65a0/giphy.gif',
-  badsteve : 'https://i.gifer.com/8oQL.gif',
-  homerhedge: 'https://media.giphy.com/media/3ohs7KViF6rA4aan5u/giphy.gif',
-  wtfhedge: 'https://i.giphy.com/media/3ohs7N0iXvc180MiB2/giphy.webp',
-  grandpahedge: 'https://media.giphy.com/media/2ALDsRipuShI8A3gXV/giphy.gif',
-  dogsoon: 'https://media.giphy.com/media/5xtDarzgzG6eu6uVwI0/giphy.gif',
-  grandpaturnaround: 'https://giphy.com/gifs/fDO2Nk0ImzvvW',
-  howdareyou: 'https://78.media.tumblr.com/tumblr_m64dbowpB71qm7tbzo1_500.gif',
-  revenge: 'https://media.giphy.com/media/6uu3nwQ1aD6t7DHotY/giphy.gif'
-};
+var macroMap = {};
 
+const lineReader = readline.createInterface({
+  input: fs.createReadStream('macros.txt')
+});
+
+lineReader.on('line', function (line){
+      var keypair = line.split(' ');
+      macroMap[_.toLower(keypair[0])] = _.toLower(keypair[1]);
+});
 
 var statusMessageArray = ['Eating stray cats to build energy.',
                           'Bathing in the blood of the innocent.', 
@@ -33,11 +33,13 @@ var bot = new discord.Client({
    token: auth.token,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
@@ -46,7 +48,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var cmd = args[0];
        
         args = args.splice(1);
-        switch(cmd) {
+        switch(cmd) 
+        {
             // !summon
             case 'summon':
                 bot.sendMessage({
